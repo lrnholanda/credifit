54d7e537e9eb439d8f1e8694d620c8f3b184e9c2
lrnholanda <lrnholanda@gmail.com>
2024-05-18 21:21:10 -0300

    emprestimo controller verbs

--- src/emprestimos/emprestimos.controller.ts
+++ src/emprestimos/emprestimos.controller.ts
@@ -11,4 +11,24 @@ export class EmprestimosController {
   create(@Body() createEmprestimoDto: CreateEmprestimoDto) {
     return this.emprestimosService.create(createEmprestimoDto);
   }
+
+  @Get()
+  findAll() {
+    return this.emprestimosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.emprestimosService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEmprestimoDto: UpdateEmprestimoDto) {
+    return this.emprestimosService.update(+id, updateEmprestimoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.emprestimosService.remove(+id);
+  }
 }
