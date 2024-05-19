cdb141bac5972d964b0345275b0049c18de34021
lrnholanda <lrnholanda@gmail.com>
2024-05-18 22:33:10 -0300

    added some test functions

--- src/emprestimos/emprestimos.service.spec.ts
+++ src/emprestimos/emprestimos.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EmprestimosService } from './emprestimos.service';
 import { HttpService } from '@nestjs/axios';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { of } from 'rxjs';
 
 describe('EmprestimosService', () => {
   let service: EmprestimosService;
@@ -32,6 +33,28 @@ describe('EmprestimosService', () => {
     prisma = module.get<PrismaService>(PrismaService);
     httpService = module.get<HttpService>(HttpService);
   });
+  it('validação do score e criar emprestimo', async () => {
+    jest.spyOn(prisma.funcionario, 'findUnique').mockResolvedValue({
+      nomeCompleto: "Carlos Oliveira",
+      cpf: "32165498700",
+      email: "carlos@empresaabc.com",
+      senha: "senha789",
+      salario: 3000,
+      empresaId: 1,
+      Empresa: {
+        id: 1, 
+        cnpj: "85685848557897",
+        razaoSocial: "Loren",
+        nomeCompleto: "Lore",
+        cpf: "6879793678796",
+        email: "holanda@gmail.com",
+        senha: "789790"
+      }
+    });
+    jest.spyOn(httpService, 'get').mockReturnValue(
+      of({ data: { score: 580 } } as any)
+    );
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
