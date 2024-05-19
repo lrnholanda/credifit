3180d47d43ffcd87fbabbbdc9654a4c60e07d69b
lrnholanda <lrnholanda@gmail.com>
2024-05-18 21:46:29 -0300

    Added test setup with mocked dependencies for EmprestimosService

--- src/emprestimos/emprestimos.service.spec.ts
+++ src/emprestimos/emprestimos.service.spec.ts
@@ -1,15 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmprestimosService } from './emprestimos.service';
+import { HttpService } from '@nestjs/axios';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('EmprestimosService', () => {
   let service: EmprestimosService;
+  let prisma: PrismaService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmprestimosService],
+      providers: [
+        EmprestimosService,
+        {
+          provide: PrismaService,
+          useValue: {
+            funcionario: { findUnique: jest.fn() },
+            emprestimo: { create: jest.fn() },
+            parcela: { createMany: jest.fn() },
+          },
+        },
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile();
-
     service = module.get<EmprestimosService>(EmprestimosService);
+    prisma = module.get<PrismaService>(PrismaService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
